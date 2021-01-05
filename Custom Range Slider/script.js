fc3f692e0705bf2f6fe49d9e04a3274e884d70c8
Vishwas Anavatti <vishwasanavatti@gmail.com>
2021-01-05 22:45:56 +0100

    Custom Range Slider

--- Custom Range Slider/script.js
+++ Custom Range Slider/script.js
@@ -1,16 +1,15 @@
 const range = document.getElementById('range');
 
 range.addEventListener('input', (e) => {
-  const value = e.target.value;
+  const value = +e.target.value;
   const label = e.target.nextElementSibling;
 
   const rangeWidth = getComputedStyle(e.target).getPropertyValue('width');
-  const labelWidth = getComputedStyle(
-    e.target.nextElementSibling
-  ).getPropertyValue('width');
+  const labelWidth = getComputedStyle(label).getPropertyValue('width');
 
+  //   converting to number
   const numRangeWidth = +rangeWidth.substring(0, rangeWidth.length - 2);
-  const numLabelWidth = +rangeWidth.substring(0, labelWidth.length - 2);
+  const numLabelWidth = +labelWidth.substring(0, labelWidth.length - 2);
 
   const max = +e.target.max;
   const min = +e.target.min;
@@ -20,7 +19,7 @@ range.addEventListener('input', (e) => {
     numLabelWidth / 2 +
     scale(value, min, max, 10, -10);
 
-  label.style.left = left - 20 + 'px';
+  label.style.left = left + 'px';
 
   label.innerHTML = value;
 });
