5c78e6c2be566d1e3897d3d76988856ec0896efd
Vishwas Anavatti <vishwasanavatti@gmail.com>
2021-01-07 23:25:20 +0100

    Insect Catch Game

--- Insect Catch Game/script.js
+++ Insect Catch Game/script.js
@@ -1,27 +1,49 @@
 const screens = document.querySelectorAll('.screen');
 const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
 const start_btn = document.getElementById('start-btn');
-const game_container = document.getElementById('game-container');
+const gameContainer = document.getElementById('game-container');
 const timeEl = document.getElementById('time');
 const scoreEl = document.getElementById('score');
 const message = document.getElementById('message');
+
 let seconds = 0;
 let score = 0;
-let selected_insect = {};
+let select_insect = {};
 
 start_btn.addEventListener('click', () => screens[0].classList.add('up'));
 
-choose_insect_btns.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    const img = btn.querySelector('img');
+choose_insect_btns.forEach((insectBtn) =>
+  insectBtn.addEventListener('click', () => {
+    const img = insectBtn.querySelector('img');
     const src = img.getAttribute('src');
     const alt = img.getAttribute('alt');
-    selected_insect = { src, alt };
+
+    select_insect = { src, alt };
+
     screens[1].classList.add('up');
+
     setTimeout(createInsect, 1000);
     startGame();
-  });
-});
+  })
+);
+
+function createInsect() {
+  const insect = document.createElement('div');
+  insect.classList.add('insect');
+
+  const { x, y } = getRandomLocation();
+
+  insect.style.left = x + 'px';
+  insect.style.top = y + 'px';
+
+  insect.innerHTML = `<img src="${select_insect.src}" alt="${
+    select_insect.alt
+  }" style="transform: rotate(${Math.random() * 360}deg)">`;
+
+  insect.addEventListener('click', catchInsect);
+
+  gameContainer.appendChild(insect);
+}
 
 function startGame() {
   setInterval(increaseTime, 1000);
@@ -30,32 +52,21 @@ function startGame() {
 function increaseTime() {
   let m = Math.floor(seconds / 60);
   let s = seconds % 60;
+
   m = m < 10 ? `0${m}` : m;
   s = s < 10 ? `0${s}` : s;
+
   timeEl.innerHTML = `Time: ${m}:${s}`;
   seconds++;
-}
-
-function createInsect() {
-  const insect = document.createElement('div');
-  insect.classList.add('insect');
-  const { x, y } = getRandomLocation();
-  insect.style.top = `${y}px`;
-  insect.style.left = `${x}px`;
-  insect.innerHTML = `<img src="${selected_insect.src}" alt="${
-    selected_insect.alt
-  }" style="transform: rotate(${Math.random() * 360}deg)" />`;
-
-  insect.addEventListener('click', catchInsect);
-
-  game_container.appendChild(insect);
 }
 
 function getRandomLocation() {
   const width = window.innerWidth;
   const height = window.innerHeight;
+
   const x = Math.random() * (width - 200) + 100;
   const y = Math.random() * (height - 200) + 100;
+
   return { x, y };
 }
 
